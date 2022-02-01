e3fc4e51fba24e6729258c5afa262191bb309677
MarvinQuiet <wenjing.ma@emory.edu>
2022-02-01 11:06:36 -0500

    Use ArchR in python

--- SlashCells/preprocess/preprocess.py
+++ SlashCells/preprocess/preprocess.py
@@ -1,4 +1,4 @@
-import rpy2
+import os, sys, glob, shutil
 import rpy2.robjects as robjects
 from rpy2.robjects.packages import importr
 
@@ -23,7 +23,8 @@ def _set_ArchR(genome: str,
 def _run_ArchR(input_files: List[str], sample_names: List[str], 
         output_dir: str = 'ArchR_output',
         min_tss: int = 4, min_frags: int = 1000,
-        add_tilemat = False, add_genescore = True):
+        add_tilemat = False, add_genescore = True,
+        save_proj = True):
     '''
     Run ArchR on input files
     ---
@@ -32,7 +33,15 @@ def _run_ArchR(input_files: List[str], sample_names: List[str],
         - sample_names: corresponding input sample names
         - min_tss: TSS enrichment score threshold to filter low quality cells
         - min_frags: number of mapped fragment threshold to filter low quality cells
+        - add_tilemat: add 500bps tile matrix to the object
+        - add_genescore: add gene score matrix to the object
+        - save_proj: save ArchR project as RDS
+
     '''
+    if len(input_files) != len(sample_names):
+        ## exit the program
+        sys.exit("Length of input files and sample names are not equal. Please make sure these two arguments are corresponding to each other.")
+
     if not os.path.exists(output_dir):
         os.makedirs(output_dir, exist_ok=True)
  
@@ -42,25 +51,60 @@ def _run_ArchR(input_files: List[str], sample_names: List[str],
             minTSS=min_tss, minFrags=min_frags, 
             addTileMat=add_tilemat, 
             addGeneScoreMat=add_genescore)
+    if len(ArchR_arrowfiles) != len(input_files):
+        ## TODO: add warning
+        pass
+
     ArchR_proj_func = robjects.r['ArchRProject']
     ArchR_proj = ArchR_proj_func(ArrowFiles=robjects.StrVector(ArchR_arrowfiles),
             outputDirectory=output_dir, copyArrows=True) ## copy everything there and delete all files generated
-    ArchR_save_func = robjects.r['saveArchRProject']
-    ArchR_save_func(ArchRProj=ArchR_proj, outputDirectory=output_dir, load=False)
+    if save_proj:
+        ArchR_save_func = robjects.r['saveArchRProject']
+        ArchR_save_func(ArchRProj=ArchR_proj, outputDirectory=output_dir, load=False)
 
     ArchR_genescore_func = robjects.r['getMatrixFromProject']
     ArchR_genescore_mat = ArchR_genescore_func(ArchR_proj, useMatrix="GeneScoreMatrix")
 
+    mm, base = importr('Matrix'), importr("base") ## import Matrix package in python
+    dollar = base.__dict__["$"]
+    mm_write_func, write_func = robjects.r['writeMM'], robjects.r['write']
+    assay_func, rowData_func, colnames_func = robjects.r['assays'], robjects.r['rowData'], robjects.r['colnames']
+    mm_write_func(dollar(assay_func(ArchR_genescore_mat), 'GeneScoreMatrix'),
+            output_dir+os.sep+'ArchR_genescore.mtx')
+    write_func(dollar(rowData_func(ArchR_genescore_mat), 'name'),
+            output_dir+os.sep+'ArchR_genescore_genes.tsv')
+    write_func(colnames_func(ArchR_genescore_mat),
+            output_dir+os.sep+'ArchR_genescore_barcodes.tsv')
 
+    ## deal with files
+    cur_dir = os.getcwd()
+    qc_dir = cur_dir+os.sep+'QualityControl'
+    if os.path.exists(qc_dir):
+        shutil.move(qc_dir, output_dir+os.sep+'QualityControl') ## check filter
+    log_dir = cur_dir+os.sep+'ArchRLogs'
+    if os.path.exists(log_dir):
+        shutil.move(log_dir, output_dir+os.sep+'ArchRLogs') ## check log for ArchR
+    tmp_dir = cur_dir+os.sep+'tmp'
+    if os.path.exists(tmp_dir) and len(os.listdir(tmp_dir)) == 0:
+        os.rmdir(tmp_dir)  ## remove tmp directories
+    else:
+        ## TODO: add warining
+        pass
 
-
-
-
+    arrowfiles_dir = output_dir+os.sep+'ArrowFiles'
+    if os.path.exists(arrowfiles_dir) and len(os.listdir(arrowfiles_dir)) != 0:
+        cur_arrowfiles = glob.glob(cur_dir+os.sep+'*.arrow')
+        for x in cur_arrowfiles: os.remove(x)  ## remove arrow files
+    else:
+        ## TODO: add warning
+        pass
 
 if __name__ == '__main__':
     data_dir = "/home/wma36/gpu/data/Mousebrain_scATACseq/dscATACseq"
 
-    import os, glob
     suffix = '.fragments.tsv.gz'
     input_files =  glob.glob(data_dir+os.sep+'*'+suffix)
     sample_names = [os.path.basename(x).replace(suffix, '') for x in input_files]
+
+    _set_ArchR(genome='mm10', seed=2022)
+    _run_ArchR(input_files, sample_names, output_dir=data_dir)
